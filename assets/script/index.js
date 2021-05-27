8a15362dcfd97d188d52ec8efa92feb9044a5034
ro <rochoagusti@gmail.com>
2021-05-27 20:00:56 -0300

    arreglando stilos

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,8 +1,17 @@
+//----menu-----
 const burger = document.getElementById('burger');
 const menu = document.getElementById('menu-ul');
 
 burger.addEventListener('click', ()=> {
     menu.classList.toggle('menu-ul_visible');
-    burger.classList.toggle('burger', 'burger_active');
+    burger.classList.toggle('burger', 'burger-close');
     //burger.classList.remove('burger');
 });
+
+
+//---favoritos
+/* const favorite= document.getElementById('favorite');
+
+favorite.addEventListener('click', () =>{
+
+}) */
