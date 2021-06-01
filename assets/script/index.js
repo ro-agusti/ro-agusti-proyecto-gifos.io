76ef4c8e3c129e9aa9b521c73a55fb2d29cab29d
ro <rochoagusti@gmail.com>
2021-06-01 18:13:06 -0300

    seguimos maquetando

--- assets/script/index.js
+++ assets/script/index.js
@@ -8,6 +8,20 @@ burger.addEventListener('click', ()=> {
     //burger.classList.remove('burger');
 });
 
+// ---- searching ----
+
+const buscaGifos = document.getElementById('buscaGifos');
+buscaGifos.addEventListener('click',()=>{
+    
+})
+
+/* const buscaGifos = document.getElementById('buscaGifos');
+const clave = 'SNJ9a5GbDjgSmOddC8ab03rQXLhxjPvS';
+let busqueda = buscaGifos.value ; 
+let cantidad = '12'; // se puede modificar hasta 25 que es lo que permite la API de gifos
+const consultarAPI = `https://api.giphy.com/v1/gifs/search?api_key=${clave}&q=${busqueda}&limit=${cantidad}`;  */
+
+
 
 //---favoritos
 /* const favorite= document.getElementById('favorite');
